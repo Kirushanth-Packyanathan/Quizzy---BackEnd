d8e53f249714acba3a61ef6aeebb6c2c1dea85bf
Kirushanth <kirushan06@gmail.com>
2024-08-27 17:49:11 +0530

    before authentication 2

--- src/main/java/com/example/quizz_portal_backend/config/SecurityConfig.java
+++ src/main/java/com/example/quizz_portal_backend/config/SecurityConfig.java
@@ -21,6 +21,7 @@ public class SecurityConfig {
 //                .formLogin(Customizer.withDefaults());
                 .oauth2Login(oauth->oauth.defaultSuccessUrl("/home"));
         return http.build();
+
     }
 
 }
