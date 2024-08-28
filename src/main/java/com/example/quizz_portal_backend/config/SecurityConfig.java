9e62aa81406093fd59899079b4ed0b83ea996340
Kirushanth <kirushan06@gmail.com>
2024-08-28 22:14:05 +0530

    before admin and student

--- src/main/java/com/example/quizz_portal_backend/config/SecurityConfig.java
+++ src/main/java/com/example/quizz_portal_backend/config/SecurityConfig.java
@@ -1,9 +1,7 @@
 package com.example.quizz_portal_backend.config;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -14,14 +12,17 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
+
     @Bean
-    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception{
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(authorizeRequests->authorizeRequests.anyRequest().authenticated())
-//                .formLogin(Customizer.withDefaults());
-                .oauth2Login(oauth->oauth.defaultSuccessUrl("/home"));
+                .authorizeHttpRequests(authorizeRequests -> authorizeRequests
+                        .requestMatchers("/api/**").permitAll() // Allows access to OAuth2 and login routes
+                        .anyRequest().authenticated() // Secures all other routes
+                )
+                .oauth2Login(oauth -> oauth
+                        .defaultSuccessUrl("http://localhost:3000/", true) // Redirect after successful login
+                );
         return http.build();
-
     }
-
 }
